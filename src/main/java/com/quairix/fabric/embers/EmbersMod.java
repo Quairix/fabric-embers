73877d05ef14c9b776aedba5eaf2edec0d91ed70
Dmitriy <qmn277@gmail.com>
2022-07-03 15:25:44 +0300

    -mod name -mod description -fix version -item with texture and tooltip

---
+++ src/main/java/com/quairix/fabric/embers/EmbersMod.java
@@ -0,0 +1,45 @@
+package com.quairix.fabric.embers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.quairix.fabric.embers.item.MystFuelItem;
+import com.simibubi.create.Create;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
+import net.minecraft.util.Identifier;
+
+public class EmbersMod implements ModInitializer {
+	public static final String ID = "embers";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+	public static final ItemGroup MYST_ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier("ember", "general"),
+			() -> new ItemStack(Blocks.COBBLESTONE));
+
+	public static final ItemGroup MYST_OTHER_GROUP = FabricItemGroupBuilder.create(
+					new Identifier("ember", "other"))
+			.icon(() -> new ItemStack(Items.BOWL))
+			.appendItems(stacks -> {
+				stacks.add(new ItemStack(Blocks.BONE_BLOCK));
+				stacks.add(new ItemStack(Items.APPLE));
+				stacks.add(PotionUtil.setPotion(new ItemStack(Items.POTION), Potions.WATER));
+				stacks.add(ItemStack.EMPTY);
+				stacks.add(new ItemStack(Items.IRON_SHOVEL));
+			})
+			.build();
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Init modId [{}] with version [{}]!", ID, Create.VERSION);
+		MystFuelItem.registerItem();
+	}
+}
---
+++ src/main/java/com/quairix/fabric/embers/item/MystFuelItem.java
@@ -0,0 +1,53 @@
+package com.quairix.fabric.embers.item;
+
+import static com.quairix.fabric.embers.EmbersMod.MYST_ITEM_GROUP;
+
+import java.util.List;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+
+public class MystFuelItem extends Item {
+
+	public static final Item MYST_FUEL_ITEM = new MystFuelItem(new FabricItemSettings()
+			.group(MYST_ITEM_GROUP)
+			.maxCount(64));
+
+	public static void registerItem() {
+		Registry.register(Registry.ITEM, new Identifier("embers", "myst_fuel_item"), MYST_FUEL_ITEM);
+		FuelRegistry.INSTANCE.add(MYST_FUEL_ITEM, 3000);
+	}
+
+	public MystFuelItem(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+		playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
+		return TypedActionResult.success(playerEntity.getStackInHand(hand));
+	}
+
+	@Override
+	public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+
+		// default white text
+		tooltip.add( new TranslatableText("item.tutorial.fabric_item.tooltip") );
+
+		// formatted red text
+		tooltip.add( new TranslatableText("item.tutorial.fabric_item.tooltip").formatted(Formatting.RED) );
+	}
+}
