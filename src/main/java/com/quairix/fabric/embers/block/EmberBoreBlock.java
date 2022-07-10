d84a0b40d491cffe89eef595641af3b9c516c4b1
Dmitriy <qmn277@gmail.com>
2022-07-10 17:27:52 +0300

    -ember bore base inventory (1 slot)

--- src/main/java/com/quairix/fabric/embers/block/EmberBoreBlock.java
+++ src/main/java/com/quairix/fabric/embers/block/EmberBoreBlock.java
@@ -2,7 +2,7 @@ package com.quairix.fabric.embers.block;
 
 import static com.quairix.fabric.embers.entity.EmberBoreBlockEntity.EMBER_BORE_BLOCK_ENTITY;
 
-import com.quairix.fabric.embers.entity.DemoBlockEntity;
+import com.quairix.fabric.embers.entity.EmberBoreBlockEntity;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -11,14 +11,22 @@ import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.Material;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 
@@ -26,18 +34,69 @@ public class EmberBoreBlock extends BlockWithEntity {
 
 	public static final Block EMBER_BORE_BLOCK = new EmberBoreBlock(FabricBlockSettings.of(Material.STONE).strength(0.3f));
 
+	protected static final VoxelShape SHAPE = VoxelShapes.cuboid(0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
+
 	public EmberBoreBlock(final Settings settings) {
 		super(settings.nonOpaque());
-	}
-
-	@Override
-	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-		return new DemoBlockEntity(pos, state);
 	}
 
 	public static void onInitialize() {
 		Registry.register(Registry.BLOCK, new Identifier("embers", "ember_bore_block"), EMBER_BORE_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("embers", "ember_bore_block"), new BlockItem(EMBER_BORE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+	}
+
+	@Override
+	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+		return new EmberBoreBlockEntity(pos, state);
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPE;
+	}
+
+	@Override
+	public ActionResult onUse(BlockState blockState, World world, BlockPos blockPos, PlayerEntity player, Hand hand, BlockHitResult blockHitResult) {
+//		if (world.isClient) return ActionResult.SUCCESS;
+		final var blockEntityBase = world.getBlockEntity(blockPos);
+
+
+		if (blockEntityBase != null || (blockEntityBase instanceof EmberBoreBlockEntity)) {
+
+			return ActionResult.FAIL;
+		}
+
+		final var blockEntity = (EmberBoreBlockEntity) blockEntityBase;
+		if (!player.getStackInHand(hand).isEmpty()) {
+			// Check what is the first open slot and put an item from the player's hand there
+			if (blockEntity.getStack(0).isEmpty()) {
+				// Put the stack the player is holding into the inventory
+				blockEntity.setStack(0, player.getStackInHand(hand).copy());
+				// Remove the stack from the player's hand
+				player.getStackInHand(hand).setCount(0);
+			} else if (blockEntity.getStack(1).isEmpty()) {
+				blockEntity.setStack(1, player.getStackInHand(hand).copy());
+				player.getStackInHand(hand).setCount(0);
+			} else {
+				// If the inventory is full we'll print it's contents
+				System.out.println("The first slot holds "
+						+ blockEntity.getStack(0) + " and the second slot holds " + blockEntity.getStack(1));
+			}
+		} else {
+			// If the player is not holding anything we'll get give him the items in the block entity one by one
+
+			// Find the first slot that has an item and give it to the player
+			if (!blockEntity.getStack(1).isEmpty()) {
+				// Give the player the stack in the inventory
+				player.getInventory().offerOrDrop(blockEntity.getStack(1));
+				// Remove the stack from the inventory
+				blockEntity.removeStack(1);
+			} else if (!blockEntity.getStack(0).isEmpty()) {
+				player.getInventory().offerOrDrop(blockEntity.getStack(0));
+				blockEntity.removeStack(0);
+			}
+		}
+		return ActionResult.SUCCESS;
 	}
 
 	@Override
@@ -47,6 +106,6 @@ public class EmberBoreBlock extends BlockWithEntity {
 
 	@Override
 	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-		return checkType(type, EMBER_BORE_BLOCK_ENTITY, (world1, pos, state1, be) ->  be.tick(world1, pos, state1, be));
+		return checkType(type, EMBER_BORE_BLOCK_ENTITY, (world1, pos, state1, be) -> be.tick(world1, pos, state1, be));
 	}
 }
--- src/main/java/com/quairix/fabric/embers/entity/EmberBoreBlockEntity.java
+++ src/main/java/com/quairix/fabric/embers/entity/EmberBoreBlockEntity.java
@@ -5,27 +5,40 @@ import static com.quairix.fabric.embers.block.DemoBlock.DEMO_BLOCK;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.quairix.fabric.embers.inventory.ImplementedInventory;
+
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SidedInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
-public class EmberBoreBlockEntity extends BlockEntity {
+public class EmberBoreBlockEntity extends BlockEntity implements ImplementedInventory, SidedInventory {
 
 	private int number = 7;
-//	private boolean isLava = false;
+
+	private final DefaultedList<ItemStack> items = DefaultedList.ofSize(1, ItemStack.EMPTY);
 	public static BlockEntityType<EmberBoreBlockEntity> EMBER_BORE_BLOCK_ENTITY;
 
 	public EmberBoreBlockEntity(@Nonnull BlockPos pos, BlockState state) {
 		super(EMBER_BORE_BLOCK_ENTITY, pos, state);
+	}
+
+	@Override
+	public DefaultedList<ItemStack> getItems() {
+		return items;
 	}
 
 	public void tick(World world, BlockPos pos, BlockState state, EmberBoreBlockEntity be) {
@@ -35,12 +48,34 @@ public class EmberBoreBlockEntity extends BlockEntity {
 //			isLava = !isLava;
 //		}
 	}
+	@Override
+	public int[] getAvailableSlots(Direction var1) {
+		// Just return an array of all slots
+		int[] result = new int[getItems().size()];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = i;
+		}
+
+		return result;
+	}
+
+	@Override
+	public boolean canInsert(int slot, ItemStack stack, Direction direction) {
+		return direction != Direction.UP;
+	}
+
+	@Override
+	public boolean canExtract(int slot, ItemStack stack, Direction direction) {
+		return true;
+	}
 
 	// Serialize the BlockEntity
 	@Override
 	public void writeNbt(NbtCompound tag) {
 		// Save the current value of the number to the tag
 		tag.putInt("number", number);
+
+		Inventories.writeNbt(tag, items);
 
 		super.writeNbt(tag);
 	}
@@ -50,6 +85,7 @@ public class EmberBoreBlockEntity extends BlockEntity {
 	public void readNbt(NbtCompound tag) {
 		super.readNbt(tag);
 
+		Inventories.readNbt(tag, items);
 		number = tag.getInt("number");
 	}
 
